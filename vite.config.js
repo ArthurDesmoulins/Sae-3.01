2c2e25cda5771cab1be72316ab0a776d6f62ded5
ArthurDesmoulins <arthur.desmoulins@etu.unilim.fr>
2025-10-23 11:15:47 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "Sae-3.01",  // ⚠️ Remplacer par le nom de votre dépôt GitHub
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        accueil: resolve(__dirname, "Composants html/Home-page.html"),
+        panier: resolve(__dirname, "Composants html/Basket.html"),
+        // Ajouter d'autres pages ici si nécessaire
+      },
+    },
+  },
 });
