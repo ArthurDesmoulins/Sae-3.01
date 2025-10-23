46c64ab5009b485a7ba171fd1443fd0fde136b31
ArthurDesmoulins <arthur.desmoulins@etu.unilim.fr>
2025-10-23 11:29:57 +0200

    re test

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "Sae-3.01",  // ⚠️ Remplacer par le nom de votre dépôt GitHub
+  base: "/Sae-3.01/",  // ⚠️ Remplacer par le nom de votre dépôt GitHub
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        accueil: resolve(__dirname, "Home-page.html"),
-        panier: resolve(__dirname, "Basket.html"),
+        accueil: resolve(__dirname, "/Sae-3.01/Home-page.html"),
+        panier: resolve(__dirname, "/Sae-3.01/Basket.html"),
         // Ajouter d'autres pages ici si nécessaire
       },
     },
